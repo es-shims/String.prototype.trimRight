47a6dfce5614172c5ed1433cc8944bccdc54f91e
Jordan Harband <ljharb@gmail.com>
2020-03-30 01:00:45 -0700

    [Refactor] use `es-abstract`’s `callBind` helper instead of `function-bind` directly

--- implementation.js
+++ implementation.js
@@ -1,9 +1,7 @@
 'use strict';
 
-var CheckObjectCoercible = require('es-abstract/5/CheckObjectCoercible');
-var ToString = require('es-abstract/2019/ToString');
-var callBound = require('es-abstract/helpers/callBound');
-var $replace = callBound('String.prototype.replace');
+var callBind = require('es-abstract/helpers/callBind');
+var $replace = callBind(String.prototype.replace);
 
 /* eslint-disable no-control-regex */
 var rightWhitespace = /[\x09\x0A\x0B\x0C\x0D\x20\xA0\u1680\u180E\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u3000\u2028\u2029\uFEFF]*$/;
