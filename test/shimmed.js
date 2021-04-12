38df440e0f4b6ffb1de5ebdfe94c5320260675b7
Jordan Harband <ljharb@gmail.com>
2021-04-11 22:02:49 -0700

    [Tests] increase coverage

--- test/shimmed.js
+++ test/shimmed.js
@@ -1,7 +1,6 @@
 'use strict';
 
-var trimRight = require('../');
-trimRight.shim();
+require('../auto');
 
 var runTests = require('./tests');
 
@@ -26,8 +25,8 @@ test('shimmed', function (t) {
 	var supportsStrictMode = (function () { return typeof this === 'undefined'; }());
 
 	t.test('bad string/this value', { skip: !supportsStrictMode }, function (st) {
-		st['throws'](function () { return trimRight(undefined, 'a'); }, TypeError, 'undefined is not an object');
-		st['throws'](function () { return trimRight(null, 'a'); }, TypeError, 'null is not an object');
+		st['throws'](function () { return String.prototype.trimRight.call(undefined, 'a'); }, TypeError, 'undefined is not an object');
+		st['throws'](function () { return String.prototype.trimRight.call(null, 'a'); }, TypeError, 'null is not an object');
 		st.end();
 	});
 
